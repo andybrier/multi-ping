749f38024d92c0cef85b4c243c5a5b08eb5271f3
chunhua, zhang <andybrier@qq.com>
2022-09-05 18:38:16 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -19,31 +19,36 @@ limitations under the License.
 
 # A small demo, which demonstrates how to use MultiPing
 
+
 from multiping import MultiPing
 from multiping import multi_ping
 import datetime
 import time
+import subprocess
+
 
 if __name__ == "__main__":
-   
-    # A list of addresses and names, which should be pinged.
-    addrs = ["110.242.68.66", "8.8.8.8", "114.114.114.114", "127.0.0.1"]
-    fileName='/tmp/logs.txt' 
+  # A list of addresses and names, which should be pinged.
+    addrs = [ "114.114.114.114", "127.0.0.1"]
+    fileName='/tmp/logs.txt'
+    cmd1 = ['tcpdump', '-i', 'eth0', '-s', '0', '-w', '/tmp/tair.pcap', '-W', '256', '-C', '1', 'dst', 'port', '9736']
+    proc = subprocess.Popen(cmd1, stdout=subprocess.PIPE)
     with open(fileName,'w')as file:
             file.write('-------')
-    
+
             for i in range(100000):
                 mp = MultiPing(addrs)
                 mp.send()
-                responses, no_response = mp.receive(0.01)
+                responses, no_response = mp.receive(0.02)
 
                 current_time = datetime.datetime.now()
 
-              
+
                 if not no_response:
                     file.write("%s all done, received responses from everyone \n" %current_time)
                 else:
                     file.write(" %s   %d. retry, resending to: %s \n" % (current_time, i + 1, no_response))
+                    proc.kill()
                 time.sleep(1)
                 
             file("done, exit")
